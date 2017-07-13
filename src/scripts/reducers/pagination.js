1d0f8a7605c22e909c0be317958c3c88414c7e45
Ion D. Filho <iondrimba@gmail.com>
2017-07-12 22:35:45 -0300

    removing logs

--- src/scripts/reducers/pagination.js
+++ src/scripts/reducers/pagination.js
@@ -3,11 +3,9 @@ import { LOCATION_CHANGE } from 'react-router-redux';
 function pagination(state = { current: 0, total: 0, pages: [] }, action) {
   switch (action.type) {
     case LOCATION_CHANGE:
-      // console.log('pagination reducer', state, action.pagination)
       var f = action.payload.pathname.split('/');
       return Object.assign({}, state, { current: f[2] || 0 });
     case PAGINATION:
-      // console.log('pagination reducer', state, action.pagination)
       return Object.assign({}, state, action.pagination);
   }
   return state;
