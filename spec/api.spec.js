2764ea4e9797a3ea5cca2e413bea9c1fed59ec82
Ion D. Filho <iondrimba@gmail.com>
2017-08-27 19:42:12 -0300

    removed outdated tests

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -7,87 +7,47 @@ describe('API tests', () => {
 
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-    //let api = {};
+    let api = {};
 
-    // beforeEach(function () {
-    //     api = new Api(API_KEY);
-    //     console.log('before', api.request.defaults.baseURL);
-    // });
+    beforeEach(function () {
+        api = new Api(API_KEY);
+    });
 
-    // afterEach(function () {
-    //     api = null;
-    // });
+    afterEach(function () {
+        api = null;
+    });
 
-    // it('Api.getCharacters - should retrive character requests', () => {
-    //     return api.getCharacters().then((response) => {
-    //         expect(response.data.code).toBe(200);
-    //     });
-    // });
-
-    // it('Api.getCharacters - should retrive character requests with parameters', () => {
-    //     return api.getCharacters({ limit: 5 }).then((response) => {
-    //         expect(response.data.code).toBe(200);
-    //         expect(response.data.data.limit).toBe(5);
-    //     });
-    // });
-
-    // it('Api.getCharacters - should retrive character by name', () => {
-    //     return api.getCharacters({ limit: 5, name: 'spider-man' }).then((response) => {
-    //         expect(response.data.code).toBe(200);
-    //         expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-man');
-    //     });
-    // });
-
-    // it('Api.appendParameters - should append options to request url', () => {
-    //     let options = {
-    //         limit: 10,
-    //         offset: 5
-    //     };
-    //     let result = api.appendParameters(api.characterUrl, options);
-    //     expect(result).toBe(`${api.characterUrl}?apikey=${api.publicKey}&limit=10&offset=5`);
-    // });
-
-    // it('Api.formatUrlWithId - should append id to request url', () => {
-    //     let id = 125;
-    //     let options = {
-    //         limit: 10,
-    //         offset: 5
-    //     };
-    //     let result = api.formatUrlWithId(api.characterUrl, id);
-    //     expect(result).toBe(`${api.characterUrl}/${id}`);
-    // });
-
-    // it('Api.getCharacterById - should retrive character by Id', () => {
-    //     let id = 1009610;
-    //     return api.getCharacterById(id, { limit: 5 }).then((response) => {
-    //         expect(response.data.code).toBe(200);
-    //         expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-man');
-    //     });
-    // });
-
-    // it('Api.getCharacterById - should retrive code 409', () => {
-    //     let id = 1009610;
-    //     return api.getCharacterById(id, { limit: 0 }).catch((error) => {
-    //         expect(error.data.code).toBe(409);
-    //     });
-    // });
-
-    it('Api.getCharacterById - should retrive error', () => {
-        let id = 1;
-        let api = new Api(API_KEY);
-        api.instance.defaults.baseURL = 'http:/aaa/';
-        return api.getCharacterById(id).catch((error) => {
-            console.log(error);
-            expect(error.stack).toBeDefined();
+    it('Api.getCharacters - should retrive character requests', () => {
+        return api.getCharacters({ page:1, orderBy: 'name', nameStartsWith: 'spi'}).then((response) => {
+            expect(response.data.code).toBe(200);
         });
     });
 
-    // it('Api.getCharacters - should retrive error', () => {
-    //     let id = 1009610;
-    //     api.version = 'v2';
-    //     return api.getCharacters({ limit: 0 }).catch((error) => {
-    //         expect(error.stack).toBeDefined();
-    //     });
-    // });
+    it('Api.getCharacters - should retrive character with offset 15', () => {
+        return api.getCharacters({ page: 2, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+            expect(response.data.code).toBe(200);
+            expect(response.data.data.offset).toBe(15);
+        });
+    });
+
+    it('Api.getCharacters - should retrive character by name that starts with spi', () => {
+        return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+            expect(response.data.code).toBe(200);
+            expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-dok');
+        });
+    });
+
+    it('Api.appendParameters - should append options to request url', () => {
+        let result = api.appendParameters(api.characterUrl, { page: 1, orderBy: 'name', nameStartsWith: 'spi' });
+        expect(result).toBe(`${api.characterUrl}?apikey=${api.publicKey}&nameStartsWith=spi&orderBy=name&limit=15&offset=0`);
+    });
+
+    it('Api.getCharacters - should retrive error', () => {
+        let id = 1009610;
+        api.version = 'v2';
+        return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).catch((error) => {
+            expect(error.stack).toBeDefined();
+        });
+    });
 
 });
