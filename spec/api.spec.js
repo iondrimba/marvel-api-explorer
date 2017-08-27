229327261258744f411e0008869da16a9cce516c
Ion D. Filho <iondrimba@gmail.com>
2017-08-27 19:42:43 -0300

    fixed spaces

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -3,51 +3,51 @@ import axios from 'axios';
 
 describe('API tests', () => {
 
-    const API_KEY = window.__karma__.config.jasmine.API_KEY;
+  const API_KEY = window.__karma__.config.jasmine.API_KEY;
 
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-    let api = {};
+  let api = {};
 
-    beforeEach(function () {
-        api = new Api(API_KEY);
+  beforeEach(function () {
+    api = new Api(API_KEY);
+  });
+
+  afterEach(function () {
+    api = null;
+  });
+
+  it('Api.getCharacters - should retrive character requests', () => {
+    return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+      expect(response.data.code).toBe(200);
     });
+  });
 
-    afterEach(function () {
-        api = null;
+  it('Api.getCharacters - should retrive character with offset 15', () => {
+    return api.getCharacters({ page: 2, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+      expect(response.data.code).toBe(200);
+      expect(response.data.data.offset).toBe(15);
     });
+  });
 
-    it('Api.getCharacters - should retrive character requests', () => {
-        return api.getCharacters({ page:1, orderBy: 'name', nameStartsWith: 'spi'}).then((response) => {
-            expect(response.data.code).toBe(200);
-        });
+  it('Api.getCharacters - should retrive character by name that starts with spi', () => {
+    return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+      expect(response.data.code).toBe(200);
+      expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-dok');
     });
+  });
 
-    it('Api.getCharacters - should retrive character with offset 15', () => {
-        return api.getCharacters({ page: 2, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
-            expect(response.data.code).toBe(200);
-            expect(response.data.data.offset).toBe(15);
-        });
-    });
+  it('Api.appendParameters - should append options to request url', () => {
+    let result = api.appendParameters(api.characterUrl, { page: 1, orderBy: 'name', nameStartsWith: 'spi' });
+    expect(result).toBe(`${api.characterUrl}?apikey=${api.publicKey}&nameStartsWith=spi&orderBy=name&limit=15&offset=0`);
+  });
 
-    it('Api.getCharacters - should retrive character by name that starts with spi', () => {
-        return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
-            expect(response.data.code).toBe(200);
-            expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-dok');
-        });
+  it('Api.getCharacters - should retrive error', () => {
+    let id = 1009610;
+    api.version = 'v2';
+    return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).catch((error) => {
+      expect(error.stack).toBeDefined();
     });
-
-    it('Api.appendParameters - should append options to request url', () => {
-        let result = api.appendParameters(api.characterUrl, { page: 1, orderBy: 'name', nameStartsWith: 'spi' });
-        expect(result).toBe(`${api.characterUrl}?apikey=${api.publicKey}&nameStartsWith=spi&orderBy=name&limit=15&offset=0`);
-    });
-
-    it('Api.getCharacters - should retrive error', () => {
-        let id = 1009610;
-        api.version = 'v2';
-        return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).catch((error) => {
-            expect(error.stack).toBeDefined();
-        });
-    });
+  });
 
 });
