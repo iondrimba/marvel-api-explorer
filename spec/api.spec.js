f4863030dba76041494c46f390efc66658969406
Ion D. Filho <iondrimba@gmail.com>
2017-09-03 11:49:05 -0300

    fixed tests

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -9,43 +9,43 @@ describe('API tests', () => {
 
   let api = {};
 
-  beforeEach(function () {
+  beforeEach(() => {
     api = new Api(API_KEY);
   });
 
-  afterEach(function () {
+  afterEach(() => {
     api = null;
   });
 
   it('Api.getCharacters - should retrive character requests', () => {
-    return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+    api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).then((response) => {
       expect(response.data.code).toBe(200);
     });
   });
 
   it('Api.getCharacters - should retrive character with offset 15', () => {
-    return api.getCharacters({ page: 2, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+    api.get({ page: 2, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).then((response) => {
       expect(response.data.code).toBe(200);
       expect(response.data.data.offset).toBe(15);
     });
   });
 
   it('Api.getCharacters - should retrive character by name that starts with spi', () => {
-    return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).then((response) => {
+    api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).then((response) => {
       expect(response.data.code).toBe(200);
       expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-dok');
     });
   });
 
   it('Api.appendParameters - should append options to request url', () => {
-    let result = api.appendParameters(api.characterUrl, { page: 1, orderBy: 'name', nameStartsWith: 'spi' });
-    expect(result).toBe(`${api.characterUrl}?apikey=${api.publicKey}&nameStartsWith=spi&orderBy=name&limit=15&offset=0`);
+    let result = api.appendParameters('/characters', { page: 1, orderBy: 'name', nameStartsWith: 'spi'});
+    expect(result).toBe(`/characters?apikey=${api.publicKey}&nameStartsWith=spi&orderBy=name&limit=15&offset=0`);
   });
 
   it('Api.getCharacters - should retrive error', () => {
     let id = 1009610;
     api.version = 'v2';
-    return api.getCharacters({ page: 1, orderBy: 'name', nameStartsWith: 'spi' }).catch((error) => {
+    api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).catch((error) => {
       expect(error.stack).toBeDefined();
     });
   });
