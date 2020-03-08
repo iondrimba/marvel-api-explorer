4075baf3d60429488ba1719694552c321a60e2aa
Lucas Pimenta <lucas.pimenta94@outlook.com>
2020-03-08 02:39:49 -0300

    refact tests using async/await, :fire: forceExit flag

--- test/api.test.js
+++ test/api.test.js
@@ -1,37 +1,70 @@
+import axios from 'axios';
 import Api from '../src/scripts/model/api';
+
+jest.mock('axios');
+
+axios.create.mockImplementationOnce(() => axios);
 
 describe('API tests', () => {
 
   const API_KEY = process.env.API_KEY;
 
-  let api = {};
+  const api = new Api(API_KEY);
 
-  beforeEach(() => {
-    api = new Api(API_KEY);
+  it('Api.getCharacters - should retrive character requests', async () => {
+    const data = {
+      response: {
+        data: {
+          code: 200
+        }
+      }
+    };
+
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+
+    await expect(api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }))
+      .resolves
+      .toEqual(data);
   });
 
-  afterEach(() => {
-    api = null;
+  it('Api.getCharacters - should retrive character with offset 15', async () => {
+    const data = {
+      response: {
+        data: {
+          code: 200,
+          data: {
+            offset: 15
+          }
+        }
+      }
+    };
+
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+
+    await expect(api.get({ page: 2, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }))
+      .resolves
+      .toEqual(data);
   });
 
-  it('Api.getCharacters - should retrive character requests', () => {
-    api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).then((response) => {
-      expect(response.data.code).toBe(200);
-    });
-  });
+  it('Api.getCharacters - should retrive character by name that starts with spi', async () => {
+    const data = {
+      response: {
+        data: {
+          code: 200,
+          data: {
+            results: [
+              { name: 'spider-dok' }
+            ]
+          }
+        }
+      }
+    };
 
-  it('Api.getCharacters - should retrive character with offset 15', () => {
-    api.get({ page: 2, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).then((response) => {
-      expect(response.data.code).toBe(200);
-      expect(response.data.data.offset).toBe(15);
-    });
-  });
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
 
-  it('Api.getCharacters - should retrive character by name that starts with spi', () => {
-    api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).then((response) => {
-      expect(response.data.code).toBe(200);
-      expect(response.data.data.results[0].name.toLowerCase()).toBe('spider-dok');
-    });
+    await expect(api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }))
+      .resolves
+      .toEqual(data);
   });
 
   it('Api.appendParameters - should append options to request url', () => {
@@ -40,11 +73,17 @@ describe('API tests', () => {
     expect(result).toBe(`/characters?apikey=${api.publicKey}&nameStartsWith=spi&orderBy=name&limit=15&offset=0`);
   });
 
-  it('Api.getCharacters - should retrive error', () => {
+  it('Api.getCharacters - should retrive error', async () => {
+    const error = {
+      stack: ''
+    };
+
+    axios.get.mockImplementationOnce(() => Promise.reject(error));
+
     api.version = 'v2';
 
-    api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }).catch((error) => {
-      expect(error.stack).toBeDefined();
-    });
+    await expect(api.get({ page: 1, orderBy: 'name', nameStartsWith: 'spi', url: '/characters' }))
+      .rejects
+      .toEqual(error)
   });
 });
